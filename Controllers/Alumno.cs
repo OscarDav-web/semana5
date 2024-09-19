8d94e6b9e52c39d360c3a440a7f4f4017a1b3df9
user.email <perezmudarrao@iestptrujillo.net>
2024-09-19 11:01:41 -0500

    Mi primer commit

---
+++ Controllers/Alumno.cs
@@ -0,0 +1,55 @@
+﻿namespace WebApi.Controllers
+{
+    public class Alumno
+    {
+        //Creando Propiedades
+        string Nombre;
+        string apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+        //Constructor sin parametros
+        public Alumno()
+        {
+            Nombre = string.Empty;
+            apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+        }
+
+        //Constructor con parametros
+        public Alumno(string pNombre, string pApellido, string pdni)
+        {
+            Nombre = pNombre;
+            apellido = pApellido;
+            dni = pdni;
+        }
+        public Alumno(string pNombre, string pApellido, string pdni, DateTime pfechanacimento)
+        {
+            Nombre = pNombre;
+            apellido = pApellido;
+            dni = pdni;
+            fechanacimiento = pfechanacimento;
+        }
+        public string DevolverNombreCopleto()
+        {
+            return Nombre + " " + apellido;
+
+
+        }
+
+        public string DevolverDocumento()
+        {
+            return dni;
+
+        }
+        public int DevolverEdad()
+        {
+            return DateTime.Now.Year - fechanacimiento.Year;
+
+        }
+    }
+}
+
+
+
